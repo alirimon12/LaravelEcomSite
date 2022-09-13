7f56d67c4eb73420ca402d6548c7c2c4ad0f53ef
faruk <alirimon5@gmail.com>
2022-09-13 19:59:33 +0600

    connected to github

---
+++ resources/views/front/order_placed.blade.php
@@ -0,0 +1,17 @@
+@extends('front/layout')
+@section('page_title','Order Placed')
+@section('container')
+
+  <!-- product category -->
+<section id="aa-product-category">
+   <div class="container">
+      <div class="row" style="text-align:center;">
+        <br/><br/><br/>
+            <h2>Your order has been placed</h2>
+            <h2>Order Id:- {{session()->get('ORDER_ID')}}</h2>
+            <p>Please Pay us to this Bkash number 01816042120</p>
+        <br/><br/><br/>
+      </div>
+   </div>
+</section>
+@endsection
